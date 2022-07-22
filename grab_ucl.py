742495983af3973a132995c6b8c61b6fa78c31d9
xiaofeng <xiaofeng.fu.15@ucl.ac.uk>
2022-07-22 23:51:47 +0800

    Add retry, exception handle, progress display

--- grab_ucl.py
+++ grab_ucl.py
@@ -6,44 +6,72 @@ import re
 import requests
 import os
 from bs4 import BeautifulSoup
+from requests.adapters import HTTPAdapter
 
+def writeData(group):
+    with open('ucl.csv','a+',encoding='utf-8') as f:
+        f.write(group[0]+','+group[1]+','+group[2]+','+group[3]+','+group[4]+'\n')
+    print("---------------------------------------")
+
+print("***** OSME Edu Programme Collector *****")
 root_url = "https://www.ucl.ac.uk/prospective-students/graduate/taught-degrees?query=&year"
+headers = {"Content-Type": "application/json",
+                    "user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+                    "Connection": "close"}
 urls = []
 programmes = []
 requirements = []
 application_dates = []
 study_modes = []
 start_dates = []
-
-resp = requests.get(root_url)
+s = requests.Session()
+s.mount('http://',HTTPAdapter(max_retries=5))
+s.mount('https://',HTTPAdapter(max_retries=5))
+try:
+    resp = s.get(root_url, headers=headers)
+except requests.exceptions.RequestException as e:
+    print("Request Failed!")
+    exit()   
 resp.encoding = 'utf-8'
 content = resp.text
+
 bs = BeautifulSoup(content, 'html.parser')
 for programme in bs.select('#programme-data-content a'):
     urls.append(programme['href'])
-    programmes.append(programme.text)
+    group = ['-','-','-','-','-']
+    group[0] = programme.text
+    programmes.append(group)
 
 for i in range(len(urls)):
-    resp = requests.get(urls[i])
+    try:
+        resp = s.get(urls[i], headers=headers, timeout=10)
+    except requests.exceptions.RequestException as e:
+        print("Request Failed!")
+        exit()
     resp.encoding = 'utf-8'
     content = resp.text
+    group = programmes[i]
+
+    print("No: "+str(i)+" programme: "+ group[0] + "\nProgress: {:.2f}%".format(100*i/len(urls)))
+
     bs = BeautifulSoup(content, 'html.parser')
-    requirement = bs.select('#entry-requirements p')[0].text
-    # print(requirement)
-    requirements.append(requirement)
+    requirement = bs.select('#entry-requirements p')
+    if (not isinstance(requirement, list) or requirement == []):
+        writeData(group)
+        continue
+    
+    group[1] = requirement[0].text
 
     tags = bs.find_all('div', class_='prog-key-info__text')
     for tag in tags:
         for child in tag.children:
             if child.text == 'Applications accepted':
                 string = tag.contents[3].text.replace('\n', '').replace('\r', '')
-                formated_string = ' '.join(string.split())
-                application_dates.append(formated_string)
+                formated_string = '"'+' '.join(string.split())+'"'
+                group[2] = formated_string
             if child.text == 'Study mode':
-                study_modes.append(tag.contents[3].text)
+                group[3] = tag.contents[3].text
             if child.text == 'Programme starts':
-                start_dates.append(tag.contents[3].text)
-
-with open('ucl.txt','w') as f:
-    for i in range(len(urls)):
-        f.write(programmes[i]+','+requirements[i]+','+application_dates[i]+','+study_modes[i]+','+start_dates[i]+'\n')
+                group[4] = tag.contents[3].text
+    writeData(group)
+print("Finish!")
